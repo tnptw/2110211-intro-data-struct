db93cf197e7f00311fcb3e2d4f0b7c29940b45dd
Thanaphum Thepwan <thanaphum.tw@gmail.com>
2021-11-17 14:59:59 +0700

    add d62_q4a_hash_max_bucket

---
+++ d62_q4a_hash_max_bucket/main.cpp
@@ -0,0 +1,21 @@
+// d62_q4a_hash_max_bucket : Hash Max Bucket Length
+#include <stdexcept>
+#include <iostream>
+#include "unordered_map.h"
+#include "student.h"
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    CP::unordered_map<int, int> m;
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x;
+        std::cin >> x;
+        m[x] = x;
+        std::cout << "max bucket length = " << m.max_bucket_length() << std::endl;
+    }
+}
+

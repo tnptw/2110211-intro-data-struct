5d6cdb8ec39c53424fe3522e2ef6bfed0338bce9
Thanaphum Thepwan <thanaphum.tw@gmail.com>
2021-11-22 11:40:27 +0700

    add d64_q4a_list_merge

---
+++ d64_q4a_list_merge/student.h
@@ -0,0 +1,22 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+#include <algorithm>
+
+template<typename T>
+void CP::list<T>::merge(CP::list<CP::list<T>> &ls) {
+    for (auto &l: ls) {
+        if (l.mSize > 0) {
+            mHeader->prev->next = l.mHeader->next;
+            l.mHeader->next->prev = mHeader->prev;
+            mHeader->prev = l.mHeader->prev;
+            l.mHeader->prev->next = mHeader;
+            l.mHeader->next = l.mHeader;
+            l.mHeader->prev = l.mHeader;
+            mSize += l.mSize;
+            l.mSize = 0;
+        }
+    }
+}
+
+#endif
